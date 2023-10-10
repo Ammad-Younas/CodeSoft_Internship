595341ca2adf6425f3a51634d732393ab87345e5
Ammad Younas <ammadyounas837@gmail.com>
2023-10-10 11:46:44 +0500

    Add 4th Project - Contact Book

---
+++ Task 5 - Contact Book/main.py
@@ -0,0 +1,193 @@
+import sys
+import sqlite3
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QLineEdit, QTreeView, QFormLayout, QRadioButton, QDialogButtonBox, QMessageBox, QHeaderView, QFileDialog, QDialog
+from PyQt5.QtGui import QStandardItemModel, QStandardItem
+import os
+
+class ContactApp(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('Contact App')
+        self.initUI()
+        self.db_conn = sqlite3.connect(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'contacts.db'))
+        self.create_table()
+        self.display_contacts()
+
+    def initUI(self):
+        layout = QVBoxLayout()
+
+        form_layout = QFormLayout()
+
+        self.name_entry = QLineEdit(self)
+        self.mobile_entry = QLineEdit(self)
+
+        form_layout.addRow('Name:', self.name_entry)
+        form_layout.addRow('Mobile Number:', self.mobile_entry)
+
+        button_layout = QHBoxLayout()
+        buttons = ['Add', 'Update', 'Delete', 'Export', 'Search', 'Reset']
+        for btn_text in buttons:
+            button = QPushButton(btn_text)
+            button.clicked.connect(self.onButtonClick)
+            button_layout.addWidget(button)
+
+        self.search_entry = QLineEdit(self)
+        self.search_entry.setPlaceholderText("Search term")
+
+        self.search_by_name = QRadioButton("Search by Name")
+        self.search_by_number = QRadioButton("Search by Number")
+        self.search_by_name.setChecked(True)
+
+        self.tree_model = QStandardItemModel()
+        self.tree_view = QTreeView(self)
+        self.tree_view.setModel(self.tree_model)
+        self.tree_view.header().setSectionResizeMode(QHeaderView.Stretch)
+        # self.tree_view.setEnabled(False)
+
+        layout.addLayout(form_layout)
+        layout.addLayout(button_layout)
+        layout.addWidget(self.search_entry)
+        layout.addWidget(self.search_by_name)
+        layout.addWidget(self.search_by_number)
+        layout.addWidget(self.tree_view)
+
+        self.setLayout(layout)
+
+    def create_table(self):
+        cursor = self.db_conn.cursor()
+        cursor.execute('''CREATE TABLE IF NOT EXISTS contacts
+                          (name TEXT,
+                          mobile TEXT)''')
+        self.db_conn.commit()
+
+    def onButtonClick(self):
+        sender = self.sender().text()
+        if sender == 'Add':
+            self.add_contact()
+        elif sender == 'Update':
+            self.update_contact()
+        elif sender == 'Delete':
+            self.delete_contact()
+        elif sender == 'Export':
+            self.export_contacts()
+        elif sender == 'Search':
+            self.search_contacts()
+        elif sender == 'Reset':
+            self.display_contacts()
+
+    def add_contact(self):
+        name = self.name_entry.text()
+        mobile = self.mobile_entry.text()
+        if name and mobile:
+            cursor = self.db_conn.cursor()
+            cursor.execute("INSERT INTO contacts (name, mobile) VALUES (?, ?)", (name, mobile))
+            self.db_conn.commit()
+            self.display_contacts()
+            self.name_entry.clear()
+            self.mobile_entry.clear()
+        else:
+            QMessageBox.warning(self, 'Warning', 'Please enter both name and mobile number.')
+
+    def update_contact(self):
+        selected_items = self.tree_view.selectionModel().selectedIndexes()
+        if not selected_items:
+            QMessageBox.warning(self, 'Warning', 'Please select a contact to update.')
+            return
+        selected_row = selected_items[0].row()
+        selected_contact_name = self.tree_model.item(selected_row, 0).text()
+        selected_contact_mobile = self.tree_model.item(selected_row, 1).text()
+
+        dialog = UpdateContactDialog(selected_contact_name, selected_contact_mobile, self)
+        if dialog.exec_():
+            updated_name, updated_mobile = dialog.get_values()
+            if updated_name and updated_mobile:
+                cursor = self.db_conn.cursor()
+                cursor.execute("UPDATE contacts SET name=?, mobile=? WHERE name=? AND mobile=?", (updated_name, updated_mobile, selected_contact_name, selected_contact_mobile))
+                self.db_conn.commit()
+                self.display_contacts()
+
+    def delete_contact(self):
+        selected_items = self.tree_view.selectionModel().selectedIndexes()
+        if not selected_items:
+            QMessageBox.warning(self, 'Warning', 'Please select a contact to delete.')
+            return
+        selected_row = selected_items[0].row()
+        selected_contact_name = self.tree_model.item(selected_row, 0).text()
+        selected_contact_mobile = self.tree_model.item(selected_row, 1).text()
+        cursor = self.db_conn.cursor()
+        cursor.execute("DELETE FROM contacts WHERE name=? AND mobile=?", (selected_contact_name, selected_contact_mobile))
+        self.db_conn.commit()
+        self.display_contacts()
+
+    def export_contacts(self):
+        filename, _ = QFileDialog.getSaveFileName(self, 'Save Contacts', '', 'VCF Files (*.vcf);;All Files (*)')
+        if filename:
+            with open(filename, 'w', encoding='utf-8') as vcf_file:
+                cursor = self.db_conn.cursor()
+                cursor.execute("SELECT * FROM contacts")
+                contacts = cursor.fetchall()
+                for contact in contacts:
+                    name, mobile = contact[0], contact[1]
+                    vcf_file.write(f'BEGIN:VCARD\n')
+                    vcf_file.write(f'FN:{name}\n')
+                    vcf_file.write(f'TEL:{mobile}\n')
+                    vcf_file.write(f'END:VCARD\n')
+
+    def search_contacts(self):
+        term = self.search_entry.text()
+        if term:
+            column = 'name' if self.search_by_name.isChecked() else 'mobile'
+            cursor = self.db_conn.cursor()
+            cursor.execute(f"SELECT * FROM contacts WHERE {column} LIKE ?", (f'%{term}%',))
+            results = cursor.fetchall()
+            self.show_search_results(results)
+
+    def show_search_results(self, results):
+        self.tree_model.clear()
+        self.tree_model.setHorizontalHeaderLabels(['Name', 'Mobile Number'])
+        for contact in results:
+            row = [QStandardItem(contact[0]), QStandardItem(contact[1])]
+            self.tree_model.appendRow(row)
+
+    def display_contacts(self):
+        self.tree_model.clear()
+        self.tree_model.setHorizontalHeaderLabels(['Name', 'Mobile Number'])
+        cursor = self.db_conn.cursor()
+        cursor.execute("SELECT * FROM contacts")
+        contacts = cursor.fetchall()
+        for contact in contacts:
+            row = [QStandardItem(contact[0]), QStandardItem(contact[1])]
+            self.tree_model.appendRow(row)
+
+
+class UpdateContactDialog(QDialog):
+    def __init__(self, name, mobile, parent=None):
+        super().__init__(parent)
+        self.setWindowTitle('Update Contact')
+        self.layout = QFormLayout()
+
+        self.name_entry = QLineEdit(self)
+        self.mobile_entry = QLineEdit(self)
+
+        self.name_entry.setText(name)
+        self.mobile_entry.setText(mobile)
+
+        self.layout.addRow('Name:', self.name_entry)
+        self.layout.addRow('Mobile Number:', self.mobile_entry)
+
+        button_box = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel)
+        button_box.accepted.connect(self.accept)
+        button_box.rejected.connect(self.reject)
+
+        self.layout.addWidget(button_box)
+        self.setLayout(self.layout)
+
+    def get_values(self):
+        return self.name_entry.text(), self.mobile_entry.text()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = ContactApp()
+    window.show()
+    sys.exit(app.exec_())
